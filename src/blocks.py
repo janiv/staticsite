1430393b988cfe01b69b9297c2d1611bc43ffe61
janiv <jani.vilkki@gmail.com>
2025-01-05 15:14:25 -0600

    Blocks

--- src/blocks.py
+++ src/blocks.py
@@ -1,3 +1,6 @@
+import re
+
+
 def markdown_to_blocks(markdown):
     res = []
     temp = markdown.split("\n\n")
@@ -7,3 +10,45 @@ def markdown_to_blocks(markdown):
         item = item.strip()
         res.append(item)
     return res
+
+def block_to_block_type(block):
+    headings_pattern = r"^[#]{1,6}[ ][\S]"
+    if(re.search(headings_pattern, block)):
+        return "heading"
+    if(block[0:3]=="```" and block[:-3]=="```"):
+        return "code"
+    
+    # Check if quote or unordered list
+    lines = block.split('\n')
+    line_count = len(lines)
+    q_count = 0
+    uo_list_count = 0
+    for line in lines:
+        if line[0] == ">":
+            q_count += 1
+        if line[:2] == "- " or line[:2]== "* ":
+            uo_list_count += 1
+    if q_count == line_count:
+        return "quote"
+    if uo_list_count == line_count:
+        return "unordered list"
+    
+    # Check if ordered list
+    is_ordered_list = True
+    numbered_list_pattern = r"^[\d]*\.[ ]"
+    prev = 0
+    for i in range(0, line_count):
+        num_match = re.search(numbered_list_pattern, lines[i])
+        if not num_match:
+            is_ordered_list = False
+            break
+        num_string = num_match.group(0)
+        num_string = num_string[:-2]
+        num_val = int(num_string)
+        if(num_val - prev != 1):
+            is_ordered_list = False
+            break
+        prev = num_val
+    if(is_ordered_list):
+        return "ordered list"
+    return "normal"
--- src/test_blocks.py
+++ src/test_blocks.py
@@ -27,3 +27,42 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
         res = ["#This is a heading"]
         ans = markdown_to_blocks(text)
         self.assertEqual(res, ans)
+
+
+    def test_block_to_block_type(self):
+        text = "# This is a heading"
+        res = "heading"
+        ans = block_to_block_type(text)
+        self.assertEqual(res, ans)
+    
+    
+    def test_block_to_block_type2(self):
+        text = "#This is a heading"
+        res = "normal"
+        ans = block_to_block_type(text)
+        self.assertEqual(res, ans)
+    
+    
+    def test_block_to_block_type3(self):
+        text = "1. This is an ordered list \n2. With at least 2 items\n3. Now three"
+        res = "ordered list"
+        ans = block_to_block_type(text)
+        self.assertEqual(res, ans)
+
+    def test_block_to_block_type4(self):
+        text = "- An unordered list\n- Looks weird\n- In this test"
+        res = "unordered list"
+        ans = block_to_block_type(text)
+        self.assertEqual(res, ans)
+
+    def test_block_to_block_quote(self):
+        text = ">Just a quote block\n>Lets hope this works\n>Yet again"
+        res = "quote"
+        ans = block_to_block_type(text)
+        self.assertEqual(res, ans)
+
+    def test_block_to_block_botched_quote(self):
+        text = ">Just a quote block\n<Lets hope this works\n>Yet again"
+        res = "normal"
+        ans = block_to_block_type(text)
+        self.assertEqual(res, ans)
